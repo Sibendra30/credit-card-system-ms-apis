8c29c056078c5e365588cfe2825b6f9c4158675b
Sibendra30 <46244484+Sibendra30@users.noreply.github.com>
2020-03-23 13:44:09 +0000

    Add files via upload

--- src/main/java/com/sapient/rest/security/AuthUtil.java
+++ src/main/java/com/sapient/rest/security/AuthUtil.java
@@ -1,29 +1,35 @@
-/*package com.sapient.rest.security;
+package com.sapient.rest.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//public class AuthUtil extends WebSecurityConfigurerAdapter{
-//
-//	@Override
-//    protected void configure(HttpSecurity http) throws Exception 
-//    {
-//        http
-//         .csrf().disable()
-//         .authorizeRequests().anyRequest().authenticated()
-//         .and()
-//         .httpBasic();
-//    }
-//	
-//	@Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) 
-//            throws Exception 
-//    {
-//        auth.inMemoryAuthentication()
-//            .withUser("admin")
-//            .password("admin");
-//    }
-//}
-*/
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class AuthUtil extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception 
+	{
+		auth.inMemoryAuthentication()
+		.withUser("user").password("{noop}password").roles("USER")
+		.and()
+		.withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+        //HTTP Basic authentication
+        .httpBasic() 
+        .and()
+        .authorizeRequests()
+        .antMatchers(HttpMethod.GET, "/credit-cards/**").hasRole("USER")
+        .antMatchers(HttpMethod.POST, "/credit-cards/**").hasRole("ADMIN")
+        .and()
+        .csrf().disable()
+        .formLogin().disable();
+	}
+
+}
